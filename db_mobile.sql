8454407c2f147ca4272b0da3d04c0e452e8a755f
divardhia <divardhiass@gmail.com>
2022-01-03 13:56:26 +0700

    tambah data vocab

--- db_mobile.sql
+++ db_mobile.sql
@@ -2,10 +2,10 @@
 -- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 07, 2021 at 05:11 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.4.14
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Jan 2022 pada 02.10
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 7.4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,13 +20,11 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_mobile`
 --
-CREATE DATABASE IF NOT EXISTS `db_mobile` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `db_mobile`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -39,31 +37,73 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id`, `nama`, `email`, `password`, `createdAt`, `updatedAt`) VALUES
 (1, 'diva ardhia', 'divardhia@gmail.com', '1234567890', '2021-12-07', '2021-12-07');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `vocab`
+--
+
+CREATE TABLE `vocab` (
+  `id` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `bindo` varchar(255) NOT NULL,
+  `bing` varchar(255) NOT NULL,
+  `kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `vocab`
+--
+
+INSERT INTO `vocab` (`id`, `gambar`, `bindo`, `bing`, `kategori`) VALUES
+(1, 'anjinglaut', 'Anjing Laut', 'Seals', 'Animal'),
+(2, 'anjing', 'Anjing', 'Dog', 'Animal'),
+(3, 'buaya', 'Buaya', 'Crocodile', 'Animal'),
+(4, 'burunghantu', 'Burung Hantu', 'Owl', 'Animal'),
+(5, 'gajah', 'Gajah', 'Elephant', 'Animal'),
+(6, 'gurita', 'Gurita', 'Octopus', 'Animal'),
+(7, 'harimau', 'Harimau', 'Tiger', 'Animal'),
+(8, 'kucing', 'Kucing', 'Cat', 'Animal'),
+(9, 'monyet', 'Monyet', 'Monkey', 'Animal'),
+(10, 'panda', 'Panda', 'Panda', 'Animal');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indeks untuk tabel `vocab`
+--
+ALTER TABLE `vocab`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `vocab`
+--
+ALTER TABLE `vocab`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
